bfae3897c7eca5616fece2a38413e9e3f3c996a5
Thiago <thiagozevedo3@gmail.com>
2024-09-30 08:26:02 -0300

    fetch all members

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,15 @@ const { Events } = require('discord.js');
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
-	execute(client) {
+	async execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		await client.guilds.cache.forEach(guild => {
+			console.log(`Guild found - ${guild.name}`);
+		});
+
+		const guild = client.guilds.cache.get('1187435395823185960');
+		await guild.members.fetch();
+
+		console.log(`Fetched ${guild.members.cache.size} members from ${guild.name}`);
 	},
 };
