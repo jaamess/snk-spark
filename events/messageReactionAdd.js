b40a34023f7c5c6a91c413c4c5b9dc3a450c34a9
Thiago <thiagozevedo3@gmail.com>
2024-09-30 08:58:01 -0300

    identify key emoji

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,20 +1,22 @@
 const { Events } = require('discord.js');
 
 module.exports = {
-	name: Events.MessageReactionAdd,
-	async execute(reaction, user) {
-		const targetChannelId = '1285236236071731270';
-		const targetMessageId = 'SPECIFIC_MESSAGE_ID';
-		const roleId = '1286327443430445123';
+    name: Events.MessageReactionAdd,
+    async execute(reaction, user) {
+        const targetChannelId = '1285236236071731270';
+        const targetMessageId = 'SPECIFIC_MESSAGE_ID';
+        const roleId = '1286327443430445123';
+        const targetEmoji = '1287552503608770600';
 
-		if (reaction.message.id !== targetMessageId || reaction.message.channel.id !== targetChannelId) return;
+        if (reaction.message.id !== targetMessageId || reaction.message.channel.id !== targetChannelId) return;
+        if (reaction.emoji.id !== targetEmoji) return;
 
-		const member = await reaction.message.guild.members.fetch(user.id);
-		const role = reaction.message.guild.roles.cache.get(roleId);
+        const member = await reaction.message.guild.members.fetch(user.id);
+        const role = reaction.message.guild.roles.cache.get(roleId);
 
-		if (!member.roles.cache.has(roleId)) {
-			await member.roles.add(role);
+        if (!member.roles.cache.has(roleId)) {
+            await member.roles.add(role);
             console.log(`Added role ${role.name} to ${member.user.tag}.`);
-		}
-	}
+        }
+    }
 };
--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -6,8 +6,10 @@ module.exports = {
 		const targetChannelId = '1285236236071731270';
 		const targetMessageId = 'SPECIFIC_MESSAGE_ID';
 		const roleId = '1286327443430445123';
+        const targetEmoji = '1287552503608770600';
 
 		if (reaction.message.id !== targetMessageId || reaction.message.channel.id !== targetChannelId) return;
+        if (reaction.emoji.id !== targetEmoji) return;
 
 		const member = await reaction.message.guild.members.fetch(user.id);
 		const role = reaction.message.guild.roles.cache.get(roleId);
