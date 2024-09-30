4d2145feca3f1e6f1bd8c638e8d3918e4e39e71a
Thiago <thiagozevedo3@gmail.com>
2024-09-30 09:02:28 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -15,8 +15,10 @@ module.exports = {
 		console.log(`Fetched ${guild.members.cache.size} members from ${guild.name}`);
 
 		// Add the emoji 1287552503608770600 to the message ID 1290281677674774540 in the channel ID 1285236236071731270
+		console.log('Adding reaction to message...');
 		const channel = guild.channels.cache.get('1285236236071731270');
 		const message = await channel.messages.fetch('1290281677674774540');
 		await message.react('1287552503608770600');
+		console.log('Added reaction to message.');
 	}
 };
